8a1a55ea0d94d2d615ecb421bf8fb777e5f03191
paul pichon <paul10_barca@hotmail.com>
2023-03-08 22:17:23 -0600

    Modelo: siguiente, y atender nuevo ticket

--- models/ticket-control.js
+++ models/ticket-control.js
@@ -3,6 +3,15 @@ const path = require('path');
 //file System para poder grabar en un archivo
 const fs   = require('fs');
 
+//clase del ticket
+class Ticket {
+    //recibimos como argumento el numero
+    constructor( numero, escritorio ) {
+        this.numero = numero;
+        this.escritorio = escritorio;
+    }
+}
+
 //clase
 class TicketControl {
 
@@ -64,8 +73,59 @@ class TicketControl {
         //grabar en el archivo
         //pasamos el getter toJson por .stringify() para transformarlo
         fs.writeFileSync( dbPath, JSON.stringify( this.toJson ));
+    }
 
+    //metodo para hacer el siguiente ticket
+    siguiente() {
+        //acumulador
+        this.ultimo += 1;
+        //crear una nueva instancia de la clase ticket
+        //recordar que el ticket pide 2 argumentos/parametros numero, escritorio
+        //y como no hay escritorio lo podemos dejar como null
+        const ticket = new Ticket( this.ultimo, null );
+        //insertar el ticket en el arreglo de tickets con .push()
+        this.ticket.push( ticket );
+        //esto se podria resumir en una sola linea
+        //this.ticket.push( new Ticket( this.ultimo, null ) );
 
+        //guardar en la base de datos
+        this.guardarDB();
+
+        //retornamos el ticket mas el numero de ticket
+        return `Ticket: ${ ticket.numero }`;
+    }
+
+    //atender ticket
+    //recibe como parametro un escritorio
+    //esta metodo puede regresar dos opciones un NULL o el TICKET que sera atendido por el escritorio enel parametro
+    atenderTicket( escritorio ) {
+        //supongamos que ya no haya tickets
+        if ( this.tickets.length === 0 ) {
+            //retornamos null, es decir no hay ningun ticket que se pueda atender
+            return null;
+        }
+
+        //pero si, si hay tickets
+        //y se elimina el elemento --> this.tickets[0]; con shift();
+        const ticket = this.tickets.shift(); //this.tickets[0];
+        //escritorio
+        ticket.escritorio = escritorio; //el escritorio que recibo com argumento
+
+        //agregamos el ticket al principio con .unshift() y enviamos el ticket que tengo
+        this.ultimos4.unshift( ticket );
+
+        //verificar que siempre sean 4 tickets
+        if ( this.ultimos4.length > 4 ) {
+            //borramos el ultimo ticket
+            //( -1, 1) ---> -1 = la ultima posicion del arreglo, 1 = solamente que corte una posicion
+            this.ultimos4.splice( -1, 1 );
+        }
+
+        //guardamos en la base de datos
+        this.guardarDB();
+        
+        //retornamos el ticket que estamos atendiendo
+        return ticket;
     }
 
 }
